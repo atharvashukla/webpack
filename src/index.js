4cac48c8f74e937fec2d1b8f0cff33c85dd91949
Atharva Shukla <atharvashukla282@gmail.com>
2020-10-19 15:45:09 +0530

    import lodash now.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
+
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
+  // Lodash, now `import`ed 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
