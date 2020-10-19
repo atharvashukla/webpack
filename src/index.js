fbd7d163edc6d6c5958e0c0013f2fea9db82aff1
Atharva Shukla <atharvashukla282@gmail.com>
2020-10-19 19:00:46 +0530

    add style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import { join } from 'lodash';
+import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now `import`ed 
   element.innerHTML = join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
