f33d267345c82c669b05b15cda0030de6a29d540
Atharva Shukla <atharvashukla282@gmail.com>
2020-10-19 19:23:51 +0530

    update rules on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,39 @@ module.exports = {
           'file-loader',
         ],
       },
+      {
+         test: /\.(csv|tsv)$/,
+         use: [
+           'csv-loader',
+         ],
+       },
+       {
+         test: /\.xml$/,
+         use: [
+           'xml-loader',
+         ],
+       },
+       {
+        test: /\.toml$/,
+        type: 'json',
+        parser: {
+          parse: toml.parse
+        }
+      },
+      {
+        test: /\.yaml$/,
+        type: 'json',
+        parser: {
+          parse: yaml.parse
+        }
+      },
+      {
+        test: /\.json5$/,
+        type: 'json',
+        parser: {
+          parse: json5.parse
+        }
+      }
     ],
   },
 };
